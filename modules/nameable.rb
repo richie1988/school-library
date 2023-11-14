f3e5f1c954154111ee21de8f52eb534f64d08fa8
richie1988 <richiessikaonga@gmail.com>
2023-11-14 23:59:31 +0200

    Notimplemneted error fixing the file Nameable

--- modules/nameable.rb
+++ modules/nameable.rb
@@ -1,5 +1,8 @@
 class Nameable
+  # This method raises NotImplementedError to ensure that subclasses
+  # implementing this class are forced to provide their own implementation
+  # for the correct_name method.
   def correct_name
-    raise NotImplementedError
+    raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
   end
 end
