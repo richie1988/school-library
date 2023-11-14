ab078fc4e60555d3e9ae15ea445c105001818f0c
richie1988 <richiessikaonga@gmail.com>
2023-11-14 22:52:54 +0200

    Class Decoration And implementation of inherit

--- modules/student.rb
+++ modules/student.rb
@@ -4,7 +4,7 @@ class Student < Person
   attr_accessor :classroom
 
   def initialize(name, age, parent_permission: true, classroom: nil)
-    super(name: name, age: age, parent_permission: parent_permission)
+    super(name, age, parent_permission: parent_permission)
     @classroom = classroom
   end
 
