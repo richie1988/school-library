d5b2653e135ac14a28c2f3601492649e92724d61
lily4178993 <nellytelli@gmail.com>
2023-11-23 11:57:53 +0000

    feat: add unit tests for Book class

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,1 +1,24 @@
+require 'rspec'
+require_relative '../modules/book'
 
+describe Book do
+  let(:book_title) { 'Goodbye, Dieting' }
+  let(:book_author) { 'Nawoon Im' }
+  let(:book) { Book.new(book_title, book_author) }
+
+  describe '#initialize' do
+    it 'creates a new book with the given title and author' do
+      expect(book.title).to eq(book_title)
+      expect(book.author).to eq(book_author)
+      expect(book.rentals).to be_empty
+    end
+  end
+
+  describe '#add_rental' do
+    it 'adds a rental to the book' do
+      rental = instance_double('Rental')
+      book.add_rental(rental)
+      expect(book.rentals).to include(rental)
+    end
+  end
+end
