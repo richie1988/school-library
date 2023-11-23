5dff3850bb71f0ab49d70579bc4e78d79fce3ebe
richie1988 <richiessikaonga@gmail.com>
2023-11-23 17:48:24 +0200

    Created testing files me and my partner

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -1,1 +1,23 @@
+require 'rspec'
+require_relative '../modules/rental'
+require_relative '../modules/book'
+require_relative '../modules/person'
 
+describe Rental do
+  let(:date) { '2023-11-15' }
+  let(:book) { instance_double('Book') }
+  let(:person) { instance_double('Person') }
+  subject { described_class.new(date, book, person) }
+
+  describe '#initialize' do
+    it 'should initialize with date, book, and person' do
+      allow(book).to receive(:add_rental)
+      allow(person).to receive(:add_rental)
+
+      expect { subject }.not_to raise_error
+
+      expect(book).to have_received(:add_rental).with(subject)
+      expect(person).to have_received(:add_rental).with(subject)
+    end
+  end
+end
--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,1 +1,18 @@
+# teacher_spec.rb
 
+require_relative '../modules/teacher'
+
+describe Teacher do
+  let(:teacher) { Teacher.new('Test Teacher', 30, 'Test Specialization') }
+
+  it 'should initialize with name, age, and specialization' do
+    expect(teacher.name).to eq('Test Teacher')
+    expect(teacher.age).to eq(30)
+    expect(teacher.specialization).to eq('Test Specialization')
+    expect(teacher.rentals).to be_empty
+  end
+
+  it 'should always be able to use the library' do
+    expect(teacher.can_use_library?).to be(true)
+  end
+end
